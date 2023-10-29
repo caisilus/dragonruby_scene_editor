29e9a3dbbbe0454d10f9ade1ff77b71ff624194c
caisilus <serangor@gmail.com>
2023-10-29 15:56:49 +0300

    Scaffold for posts

--- spec/features/test_feature_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "TestFeatures", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
-
-  scenario "test rspec works" do
-    expect(true).to equal(false)
-  end
-end
